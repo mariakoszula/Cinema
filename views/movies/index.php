359d9e47635f7d7ef4e6730071eb2bfdef11539f
Maria Koszucka <koszucka.maria@gmail.com>
2012-11-07 15:31:01 +0100

    testing git

---
+++ views/movies/index.php
@@ -0,0 +1,18 @@
+Filmy
+<!-- <?php
+echo "<table>";
+foreach ($this->listOfRooms as $key => $value){
+		echo "<tr>";
+		echo "<td>".$value['name']."</td>";
+		echo "<td><a href='".URL."rooms/seats/".$value['id']."'>Rozkład siedzeń</a> <a href='".URL."rooms/delete/".$value['id']."'>Usuń</a></td>";
+		echo "</tr>";
+		}
+echo "</table>";
+?>
+
+
+<form id="room_form" method="POST" action="<?php echo URL;?>rooms/add">
+	<label>Nazwa sali: </label><input type="text" name="name" required="required"/><br/>
+	<input type="submit" value="Dodaj" name="submit"/>
+</form>	
+
