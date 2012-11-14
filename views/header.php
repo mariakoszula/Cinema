1372a2cd1bac970dea58ae365421b7709b8dba34
Maria Koszucka <koszucka.maria@gmail.com>
2012-11-14 19:41:32 +0100

    view

--- views/header.php
+++ views/header.php
@@ -4,44 +4,62 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Cinema</title>
+<link rel="stylesheet" href="<?php echo URL;?>public/Style/default.css" type="text/css"/>
 <script type="text/javascript" src="<?php echo URL;?>public/Scripts/jquery.js"></script>
 <script type="text/javascript" src="<?php echo URL;?>/views/rooms/js/default.js"></script>
 <script type="text/javascript" src="<?php echo URL;?>/views/showing/js/default.js"></script>
+<script src="public/Scripts/jquery-1.4.2.min.js" type="text/javascript"></script>
 </head>
 
 <body>
 <?php session_start();?>
-<div id="header">
-
-	<a href="<?php echo URL;?>index">Dzisiejsze seanse</a>
-	<?php if($_SESSION['loggedIn']==false):?>
-	<a href="<?php echo URL;?>register">Zarejstruj się</a>
-	<?php endif;?>
-	<?php
-			switch($_SESSION['role']){
+<div = "main">
+	<div id="header">
+			<div class="row-1"><h1>Kino</h1>
+				<ul>
+				<?php if($_SESSION['loggedIn']==false):?>
+					<li><a href="<?php echo URL;?>register">Rejstracja</a></li>
+					<?php endif;?>
+					<?php if($_SESSION['role']=='client'):?>
+					<li><a href="<?php echo URL;?>login/edit">Edytuj profil</a></li>
+					<?php endif;?>
+					<?php	if($_SESSION['loggedIn']==true){
+					echo "<li><a href='".URL."manager/logout'>Wyloguj</a></li>";
+					}else
+					echo "<li><a href='".URL."login'>Zaloguj</a></li>";?>
+					
+				</ul>
+			</div>
+			<div class="nav">
+				<ul>
+					<li class="current_page"><a href="<?php echo URL;?>index">Strona Główna</a></li>
+		
+					<?php
+					switch($_SESSION['role']){
 				case "manager": 
-					echo "<a href='".URL."manager/listOfUsers'>Baza użytkowników</a>";
-					echo "<a href='".URL."manager'>Dodawanie użytkowników</a>";
-					echo "<a href='".URL."rooms'>Sale</a>";
-					echo "<a href='".URL."manageShows'>Zarządzenie repertuarem</a>";
+					echo "<li><a href='".URL."login/edit'>Wybierz seans</a></li>";
+					echo "<li><a href='".URL."'>Ranking filmów</a></li>";
+					echo "<li><a href='".URL."manager/listOfUsers'>Baza użytkowników</a></li>";
+					echo "<li><a href='".URL."manager'>Dodawanie użytkowników</a></li>";
+					echo "<li><a href='".URL."rooms'>Sale</a></li>";
+					echo "<li><a href='".URL."manageShows'>Zarządzenie repertuarem</a></li>";
 					break;
 				case "worker":
-				 	echo "<a href='".URL."manageShows'>Zarządzenie repertuarem</a>";
+					echo "<li><a href='".URL."login/edit'>Wybierz seans</a></li>";
+				 	echo "<li><a href='".URL."manageShows'>Zarządzenie repertuarem</a></li>";
+				 	echo "<li><a href='".URL."login/edit'>Edytuj profil</a></li>";
 					break;
 				case "client":
-					echo "<a href='".URL."login/edit'>Wybierz seans</a>";
-					echo "<a href='".URL."'>Ranking filmów</a>";
-					echo "<a href='".URL."'>Moje konto</a>";
-					echo "<a href='".URL."login/edit'>Edytuj profil</a>";
+					echo "<li><a href='".URL."login/edit'>Wybierz seans</a></li>";
+					echo "<li><a href='".URL."'>Ranking filmów</a></li>";
+					echo "<li><a href='".URL."'>Moje konto</a></li>";
 					break;
-			}
-	if($_SESSION['loggedIn']==true){
-		echo "<a href='".URL."manager/logout'>Wyloguj</a>";
-	}else
-		echo "<a href='".URL."login'>Zaloguj się</a>";?>
-	
+					}
+				?>
+			</ul>
+		</div>
 </div>
+<div style="clear: both;">&nbsp;</div>
 
 <div id="content">
-
 
