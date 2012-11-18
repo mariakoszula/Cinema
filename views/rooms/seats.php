7357a34540430447d3f5a743abb4c2422b0ebc1d
Maria Koszucka <koszucka.maria@gmail.com>
2012-11-18 23:32:58 +0100

    adding_rooms_works

--- views/rooms/seats.php
+++ views/rooms/seats.php
@@ -1,5 +1,3 @@
-
-
 <h1><?php echo $this->seats['name']?></h1>
 <form action="<?php echo URL?>rooms/save" method="post" name="seats">
 
@@ -7,11 +5,11 @@
 
 <p style="text-align: center;"><strong>Ekran</strong></p>
 <hr>
-<table>
+<table style="align: center">
 <?php
 
 for($i=0; $i<sizeof($this->seats)-2; $i++){
-	
+
 	if(($this->seats[$i]['row_no'])==($this->seats[$i+1]['row_no']+1) ){
 		echo "<tr>";
 	}
@@ -24,6 +22,7 @@ echo "<td>";
 }
 ?>
 </table>
+<input type="hidden" name='id' value='<?php echo $this->seats['id']?>'/>
 <button type="submit">Zapisz</button>
 </form>
 <p id="info"></p>
