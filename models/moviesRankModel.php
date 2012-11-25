346a88b28d7b69337d7601cc6035bea1f1e19909
Maria Koszucka <koszucka.maria@gmail.com>
2012-11-25 19:39:10 +0100

    ranking initial ok

--- models/moviesRankModel.php
+++ models/moviesRankModel.php
@@ -6,17 +6,62 @@ class MoviesRankModel{
 	
 	public function index(){
 		require_once "bootstrap.php";
-		$qb = $em->createQueryBuilder();
+		$dql = 'SELECT m.id, m.title, coalesce(m.rating, 0) AS rating, count(r.id) AS votes FROM Movie m LEFT JOIN Rate r WITH m.id = r.movie group BY m.id, m.title, ' .
+				'rating order by rating DESC';
+		$query = $em -> createQuery($dql);
+		$results = $query->getResult();
+	/*	$qb = $em->createQueryBuilder();
 		$qb -> select('m.id, m.title, m.rating', $qb->expr()->count('r.id'))
 			-> from('Movie', 'm')
 			-> leftJoin('Rate', 'r', 'WITH', 'm.id = r.movie')
 			-> groupBy('m.id')
 			-> addGroupBy('m.title')
 			-> addGroupBy('m.rating')
-			-> orderBy ('m.rating');
+			-> orderBy ('m.rating', DESC);
 		$query = $qb->getQuery();
-		$results = $query->getResult();
+		$results = $query->getResult();*/
 		return $results;
 	}
+	
+	public function movie($id){
+		require_once "bootstrap.php";
+		$movie = $em->find('Movie', $id);
+		$mid = $movie->getId();
+		$title = $movie->getTitle();
+		$time = $movie->getRuntime();
+		$desc = $movie->getDesc();
+
+		$user = $_SESSION['user'];
+		
+		$qb = $em->createQueryBuilder();
+		$qb -> select('r.rate', $qb -> expr()-> count('r.id'))
+			-> from('Rate', 'r')
+			-> where('r.user = ?1 AND r.movie = ?2')
+			-> groupBy('r.rate')
+			-> setParameters(array( 1 => $user, 2 => $mid));
+		$query = $qb->getQuery();
+		$single = $query->getResult();
+		$data = array(
+				'title' => $title,
+				'time' => $time,
+				'mid' => $mid,
+				'desc' => $desc,
+				'rated' => $single
+		);
+		$em->clear();
+		return $data;
+	}
+	
+	public function save_rate($data){
+		require_once "bootstrap.php";
+		$user = $em ->  find('Users', $data['user']);
+		$movie = $em -> find('Movie', $data['movie']);
+		$rate = new Rate($data['rate'], $user, $movie);
+		$em->persist($rate);
+		$em->flush();
+		$em->clear();
+		
+	}
+	
 
 }
--- views/moviesRank/index.php
+++ views/moviesRank/index.php
@@ -1,6 +1,14 @@
-
-Ranking filmów
 <table class="center">
-	<tr><th>Miejsce</th><th>Tytuł</th><th>Ocena</th><th>Ilość głosów</th>
-	<?php print_r($this->index);?>
+	<tr><th>Miejsce</th><th>Tytuł</th><th>Ocena</th><th>Ilość głosów</th><th>Na ekranie</th></tr>
+	<?php 
+	print_r($this->index);
+for($i=0; $i<sizeof($this->index); $i++){
+	
+	echo "<tr><td>".($i+1)."</td>";
+	echo "<td><a href='".URL."moviesRank/movie/".$this->index[$i]['id']."'>".$this->index[$i]['title']."</a></td>";
+	echo "<td>".$this->index[$i]['rating']."</td>";
+	echo "<td>".$this->index[$i]['votes']."</td>";
+	echo "</tr>";
+}
+?>
 </table>
